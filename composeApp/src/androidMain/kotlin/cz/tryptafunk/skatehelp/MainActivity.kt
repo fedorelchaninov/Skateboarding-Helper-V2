3ff470e6ce1f3865bf62ea2e53f80514cd7d4db7
Elchaninov Fedor <fedor.elchaninov@tul.cz>
2025-01-03 19:40:23 +0100

    cleanup

--- composeApp/src/androidMain/kotlin/cz/tryptafunk/skatehelp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/cz/tryptafunk/skatehelp/MainActivity.kt
@@ -6,14 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.LaunchedEffect
-import cz.tryptafunk.skatehelp.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            // Remove when https://issuetracker.google.com/issues/364713509 is fixed
             LaunchedEffect(isSystemInDarkTheme()) {
                 enableEdgeToEdge()
             }
